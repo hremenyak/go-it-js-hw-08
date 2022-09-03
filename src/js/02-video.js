26fd3ddda41a23e7d046b452566b8589442259df
hremenyak <hremenyak@gmail.com>
2022-09-03 15:15:40 +0200

    completing task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlayerPlay, 1000));
+
+function onPlayerPlay(event) {
+  const timeData = event.seconds;
+  localStorage.setItem('videoplayer-current-time', timeData);
+}
+
+const lastSessionTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(lastSessionTime);
