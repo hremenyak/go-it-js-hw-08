52e01bc72f5a250364da06632519a4cc922dfa79
hremenyak <hremenyak@gmail.com>
2022-09-03 15:34:00 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+// import throttle from 'lodash.throttle';
+console.log('text');
 
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  console.log(event);
+
+  localStorage.setItem('feedback-form-state');
+}
