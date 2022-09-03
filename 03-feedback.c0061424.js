3ddce1a25923833d26f724e28d23ce34bd959814
hremenyak <100085779+hremenyak@users.noreply.github.com>
2022-09-03 13:34:25 +0000

    Deploying to gh-pages from @ hremenyak/go-it-js-hw-08@52e01bc72f5a250364da06632519a4cc922dfa79 🚀

---
+++ 03-feedback.c0061424.js
@@ -0,0 +1,2 @@
+console.log("text"),document.querySelector(".feedback-form").addEventListener("input",(function(e){console.log(e),localStorage.setItem("feedback-form-state")}));
+//# sourceMappingURL=03-feedback.c0061424.js.map
