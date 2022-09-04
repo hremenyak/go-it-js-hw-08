7f662eb3f5b5e7562694a86a0fff0caacfe65525
hremenyak <100085779+hremenyak@users.noreply.github.com>
2022-09-04 05:04:18 +0000

    Deploying to gh-pages from @ hremenyak/go-it-js-hw-08@4bd9c1c0c99adfc1b30a5827f405945b7f12a6c2 🚀

--- 03-feedback.c0061424.js
+++
@@ -1,2 +0,0 @@
-console.log("text"),document.querySelector(".feedback-form").addEventListener("input",(function(e){console.log(e),localStorage.setItem("feedback-form-state")}));
-//# sourceMappingURL=03-feedback.c0061424.js.map
--- 03-feedback.d08f7d2c.js
+++
@@ -1,2 +0,0 @@
-console.log("text");document.querySelector(".feedback-form").addEventListener("input",(function(e){console.log(e),localStorage.setItem("feedback-form-state")}));
-//# sourceMappingURL=03-feedback.d08f7d2c.js.map
