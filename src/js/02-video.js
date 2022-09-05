ffb0bc85de9ddadb2a0de822020873dabc1a8d19
hremenyak <hremenyak@gmail.com>
2022-09-05 06:47:03 +0200

    fixing task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,31 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
+
 const player = new Player(iframe);
+
 player.on('timeupdate', throttle(onPlayerPlay, 1000));
 
 function onPlayerPlay(event) {
-  const timeData = event.seconds;
+  let timeData = event.seconds;
   localStorage.setItem('videoplayer-current-time', timeData);
 }
 
-const lastSessionTime = localStorage.getItem('videoplayer-current-time');
+let lastSessionTime = localStorage.getItem('videoplayer-current-time');
 
-player.setCurrentTime(lastSessionTime);
+player
+  .setCurrentTime(lastSessionTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
